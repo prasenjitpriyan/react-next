a5da460fa90fbedb8407e95801be52808ab6a06d
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2025-08-30 17:48:23 +0530

    New Project started

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="min-h-[100svh] w-[100svw] bg-gradient-to-br from-[#56c5dc] to-[#010100]">
+      Home
+    </div>
+  );
+};
+
+export default Home;
